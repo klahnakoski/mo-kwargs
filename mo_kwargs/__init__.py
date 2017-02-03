cd9c6b43c9075a916a273e2fa1ca69ebcab937a3
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-02-03 10:02:29 -0500

    change to kwargs

--- mo_kwargs/__init__.py
+++ mo_kwargs/__init__.py
@@ -18,16 +18,16 @@ from mo_dots import zip as dict_zip
 
 def override(func):
     """
-    THIS DECORATOR WILL PUT ALL PARAMETERS INTO THE `settings` PARAMETER AND
-    PUT ALL `settings` PARAMETERS INTO THE FUNCTION PARAMETERS.  THIS HAS BOTH
-    THE BENEFIT OF HAVING ALL PARAMETERS IN ONE PLACE (settings) AND ALL
+    THIS DECORATOR WILL PUT ALL PARAMETERS INTO THE `kwargs` PARAMETER AND
+    PUT ALL `kwargs` PARAMETERS INTO THE FUNCTION PARAMETERS.  THIS HAS BOTH
+    THE BENEFIT OF HAVING ALL PARAMETERS IN ONE PLACE (kwargs) AND ALL
     PARAMETERS ARE EXPLICIT FOR CLARITY.
 
     OF COURSE, THIS MEANS PARAMETER ASSIGNMENT MAY NOT BE UNIQUE: VALUES CAN
-    COME FROM EXPLICIT CALL PARAMETERS, OR FROM THE settings PARAMETER.  IN
+    COME FROM EXPLICIT CALL PARAMETERS, OR FROM THE kwargs PARAMETER.  IN
     THESE CASES, PARAMETER VALUES ARE CHOSEN IN THE FOLLOWING ORDER:
     1) EXPLICT CALL PARAMETERS
-    2) PARAMETERS FOUND IN settings
+    2) PARAMETERS FOUND IN kwargs
     3) DEFAULT VALUES ASSIGNED IN FUNCTION DEFINITION
     """
 
@@ -37,10 +37,10 @@ def override(func):
     else:
         defaults = {k: v for k, v in zip(reversed(params), reversed(func.func_defaults))}
 
-    if "settings" not in params:
-        # WE ASSUME WE ARE ONLY ADDING A settings PARAMETER TO SOME REGULAR METHOD
+    if "kwargs" not in params:
+        # WE ASSUME WE ARE ONLY ADDING A kwargs PARAMETER TO SOME REGULAR METHOD
         def w_settings(*args, **kwargs):
-            settings = kwargs.get("settings")
+            settings = kwargs.get("kwargs")
 
             params = func.func_code.co_varnames[:func.func_code.co_argcount]
             if not func.func_defaults:
@@ -55,22 +55,22 @@ def override(func):
 
     def wrapper(*args, **kwargs):
         try:
-            if func.func_name in ("__init__", "__new__") and "settings" in kwargs:
-                packed = params_pack(params, kwargs, dict_zip(params[1:], args[1:]), kwargs["settings"], defaults)
+            if func.func_name in ("__init__", "__new__") and "kwargs" in kwargs:
+                packed = params_pack(params, kwargs, dict_zip(params[1:], args[1:]), kwargs["kwargs"], defaults)
                 return func(args[0], **packed)
             elif func.func_name in ("__init__", "__new__") and len(args) == 2 and len(kwargs) == 0 and isinstance(args[1], Mapping):
-                # ASSUME SECOND UNNAMED PARAM IS settings
+                # ASSUME SECOND UNNAMED PARAM IS kwargs
                 packed = params_pack(params, args[1], defaults)
                 return func(args[0], **packed)
             elif func.func_name in ("__init__", "__new__"):
-                # DO NOT INCLUDE self IN SETTINGS
+                # DO NOT INCLUDE self IN kwargs
                 packed = params_pack(params, kwargs, dict_zip(params[1:], args[1:]), defaults)
                 return func(args[0], **packed)
-            elif params[0] == "self" and "settings" in kwargs:
-                packed = params_pack(params, kwargs, dict_zip(params[1:], args[1:]), kwargs["settings"], defaults)
+            elif params[0] == "self" and "kwargs" in kwargs:
+                packed = params_pack(params, kwargs, dict_zip(params[1:], args[1:]), kwargs["kwargs"], defaults)
                 return func(args[0], **packed)
             elif params[0] == "self" and len(args) == 2 and len(kwargs) == 0 and isinstance(args[1], Mapping):
-                # ASSUME SECOND UNNAMED PARAM IS settings
+                # ASSUME SECOND UNNAMED PARAM IS kwargs
                 packed = params_pack(params, args[1], defaults)
                 return func(args[0], **packed)
             elif params[0] == "self":
@@ -80,12 +80,12 @@ def override(func):
                 # ASSUME SINGLE PARAMETER IS A SETTING
                 packed = params_pack(params, args[0], defaults)
                 return func(**packed)
-            elif "settings" in kwargs and isinstance(kwargs["settings"], Mapping):
-                # PUT args INTO SETTINGS
-                packed = params_pack(params, kwargs, dict_zip(params, args), kwargs["settings"], defaults)
+            elif "kwargs" in kwargs and isinstance(kwargs["kwargs"], Mapping):
+                # PUT args INTO kwargs
+                packed = params_pack(params, kwargs, dict_zip(params, args), kwargs["kwargs"], defaults)
                 return func(**packed)
             else:
-                # PULL SETTINGS OUT INTO PARAMS
+                # PULL kwargs OUT INTO PARAMS
                 packed = params_pack(params, kwargs, dict_zip(params, args), defaults)
                 return func(**packed)
         except TypeError, e:
@@ -110,7 +110,7 @@ def params_pack(params, *args):
             if k in settings:
                 continue
             settings[k] = v
-    settings["settings"] = settings
+    settings["kwargs"] = settings
 
     output = {str(k): settings[k] for k in params if k in settings}
     return output
